6aca0d4b7e39c1a0fa2909c6387bf94d5225f7fa
Ali <alif.taqi@hotmail.com>
2020-10-22 21:26:53 -0700

    refac

---
+++ README-NATE.txt
@@ -0,0 +1,15 @@
+Thursday, Oct 22
+
+Hello Nate!
+
+I’ve refactored my thesis directory so to save you some time, here are the more interesting files for you to look at:
+
+- matrices.R
+- eigenvectors.R
+- eigenmetrics.R
+
+- random-matrices.pdf
+- sparsity.pdf
+- eigenmetrics.pdf
+
+-Ali
---
+++ code/R/eigenmetrics.R
@@ -0,0 +1,37 @@
+
+# This script includes functions that are metrics for analyzing eigenvectors.
+
+# The inputs for these metric functions have been standardized to the eigen_frame object returned 
+# by the eigen_frame() function in eigenvectors.R.
+
+#=================================================================================#
+#                               EIGENVECTOR METRICS
+#=================================================================================#
+
+rmt_summary <- function(P){
+  evs <- eigen(P)[1]$values
+  RMThreshold::rm.spacing.distribution(evs)
+  RMThreshold::rm.ev.density(evs)
+}
+
+eigen_summary <- function(eigen_frame){
+  prop <- prop_real_rows(eigen_frame)
+  print(prop)
+  real_prop <- round(sum(prop$is_real)/length(prop$is_real),10)
+  paste("Proportion of real-valued rows: ",real_prop,sep="")
+}
+
+prop_real_rows <- function(eigen_frame){
+  M <- sqrt(length(eigen_frame$Re)) #obtain number of rows
+  eigen_frame %>%
+    mutate(Im_0 = case_when(Im == 0 ~ T, Im != 0 ~ F)) %>%
+    group_by(row_i) %>%
+    summarize(prop_reals = round(sum(Im_0)/M, 4)) %>%
+    mutate(is_real = case_when(prop_reals == 1 ~ T, prop_reals < 1 ~ F))
+}
+
+norms <- function(eigen_frame){
+  #returns maximum norm by component, maximum norm by row,
+  #average norm of component, row
+  1
+}
