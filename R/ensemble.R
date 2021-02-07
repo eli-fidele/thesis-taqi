b3aae1dd808aaf3ed448e1a8742d5c04aa7a00b0
ataqi23 <alif.taqi00@gmail.com>
2021-02-06 18:57:13 -0800

    writeup

--- R/ensemble.R
+++ R/ensemble.R
@@ -1,4 +1,25 @@
 
+
+#=================================================================================#
+#                             RANDOM MATRIX ENSEMBLES 
+#=================================================================================#
+
+# Random matrix ensemble
+## FIX: generalize to multiple arguments rather than just dimensions, accounting for defaults
+RM_ensemble <- function(mat_type, args, size){
+  # If the mat_type entry is not the function itself, parse the mat_type string
+  if(class(mat_type) == "function"){
+    fxn <- mat_type
+  } else{
+    fxn <- parse_RMfxn(mat_type)  
+  }
+  # Replicate with appopriate number of arguments by using parse_args
+  ensemble <- replicate(n = size, expr = parse_args(fxn, args), simplify = F)
+  # Return the ensemble
+  ensemble
+}
+
+
 #=================================================================================#
 #                               BATCH EXECUTION 
 #=================================================================================#
@@ -31,41 +52,18 @@ run_class0 <- function(P, batch, stack_args = list(20, TRUE)){
 }
 
 #=================================================================================#
-#                             RANDOM MATRIX ENSEMBLES 
-#=================================================================================#
-
-# Random matrix ensemble
-## FIX: generalize to multiple arguments rather than just dimensions, accounting for defaults
-RM_ensemble <- function(mat_type, args, size){
-  # If the mat_type entry is not the function itself, parse the mat_type string
-  if(class(mat_type) == "function"){
-    fxn <- mat_type
-  } else{
-    fxn <- parse_RMfxn(mat_type)  
-  }
-  # Get number of arguments
-  n_args <- length(args)
-  if(n_args == 1){
-    ensemble <- replicate(n = size, expr = fxn(args[[1]]), simplify = F)
-  }
-  if(n_args == 2){
-    ensemble <- replicate(n = size, expr = fxn(args[[1]], args[[2]]), simplify = F)
-  }
-  if(n_args == 3){
-    ensemble <- replicate(n = size, expr = fxn(args[[1]], args[[2]], args[[3]]), simplify = F)
-  }
-  if(n_args == 4){
-    ensemble <- replicate(n = size, expr = fxn(args[[1]], args[[2]], args[[3]], args[[4]]), simplify = F)
-  }
-  # Replicate with appopriate number of arguments
-  # Return the ensemble
-  ensemble
-}
-
-#=================================================================================#
 #                                  LOW-LEVEL 
 #=================================================================================#
 
+
+parse_args <- function(fxn, args){
+  # Get number of arguments
+  n_args <- length(args)
+  if(n_args == 1){fxn(args[[1]])}
+  if(n_args == 2){fxn(args[[1]], args[[2]])}
+  if(n_args == 3){fxn(args[[1]], args[[2]], args[[3]])}
+  if(n_args == 4){fxn(args[[1]], args[[2]], args[[3]], args[[4]])}
+}
 
 # Take a list of arguments, and an index holding a vector to properly return a vector of the arguments
 # Steps: get the length of the vector argument, then do args[[i]][j] for j in 1:len(vec)
