9d95f6ad965d24867014bc751263d77a07a81ed5
= <alif.taqi00@gmail.com>
2021-02-19 23:36:47 -0800

    Update matrices-diagnostics.R

--- R/matrices-diagnostics.R
+++ R/matrices-diagnostics.R
@@ -1,10 +1,22 @@
 #==========================================================================#
-#                         RANDOM MATRIX DIAGNOSTICS 
+#                         RANDOM MATRIX DIAGNOSTICS
 #==========================================================================#
 
 # Visualize the entries of a matrix as a histrogram
 .visualize_entries <- function(P){
-  ggplot() + geom_histogram(data = data.frame(x = as.vector(P)), aes(x = x))
+  # Vectorize the matrix into a row vector of its entries
+  entries <- as.vector(P)
+  if(class(entries) == "complex"){
+    entries <- data.frame(entries = c(Re(entries), Im(entries)))
+  } else{
+    entries <- data.frame(entries = entries)
+  }
+  # Plot the histogram of its entries
+  entries_hist <-
+    ggplot(data = entries) +
+    geom_histogram(bins = 20, aes(x = entries, y = stat(density)))
+  # Return plot
+  entries_hist
 }
 
 #========================================================================#
@@ -12,35 +24,38 @@
 #========================================================================#
 
 .normal_params <- function(entries){
+  if(class(entries) == "data.frame"){entries <- entries[[1]]}
   # Vectorize the matrix into a row vector of its entries
-  if(class(entries) == "matrix"){
-    elements_P <- data.frame(x = as.vector(P))
-    entries <- data.frame(x = c(Re(elements_P$x), Im(elements_P$x)))
-    }
-  print(paste("Mean: ",round(mean(entries$x),3),sep=""))
+  print(paste("Mean: ",round(mean(entries),3),sep=""))
   print(paste("Standard Deviation: ",round(sqrt(var(entries)),3),sep=""))
 }
 
 # For a given normal matrix, visualize its entries as a histogram
 .visualize_normal_entries <- function(P){
   # Vectorize the matrix into a row vector of its entries
-  elements_P <- data.frame(x = as.vector(P))
-  entries_P <- data.frame(x = c(Re(elements_P$x), Im(elements_P$x)))
-  mu <- mean(entries_P$x, na.rm = T)
-  sd <- sqrt(var(entries_P$x, na.rm = T))
-  .normal_params(entries_P)
+  entries <- as.vector(P)
+  if(class(entries) == "complex"){
+    entries <- data.frame(entries = c(Re(entries), Im(entries)))
+  } else{
+    entries <- data.frame(entries = entries)
+  }
+  # Get parameters
+  mu <- mean(entries[[1]], na.rm = T)
+  sd <- sqrt(var(entries[[1]], na.rm = T))
+  .normal_params(entries) # Print parameters
   # Get theoretical distribution function
   normal_density <- function(x){dnorm(x, mean = mu, sd = sd)}
   # Plot the histogram of its entries
-  entries_hist <- ggplot(data = entries_P, mapping = aes(x)) + 
-    geom_histogram(bins = 20, aes(y = stat(density))) +
+  entries_hist <-
+    ggplot(data = entries) +
+    geom_histogram(bins = 20, aes(x = entries, y = stat(density))) +
     stat_function(fun = normal_density)
   # Return plot
   entries_hist
 }
 
 #=========================================================================#
-#                 STOCHASTIC RANDOM MATRIX DIAGNOSTICS 
+#                 STOCHASTIC RANDOM MATRIX DIAGNOSTICS
 #=========================================================================#
 
 # Print out the sums of each of the rows (to check row-stochasticity)
